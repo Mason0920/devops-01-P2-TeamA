3c299afecaeefdb6f8c90781bad8d7e3ef41ef71
이희성 <masonjar@ihuiseong-ui-MacBookPro.local>
2022-04-04 17:45:36 +0900

    ADD:project2-restaurant

--- routes/orders/read.js
+++ routes/orders/read.js
@@ -2,13 +2,10 @@
 const { readAll } = require('../../model')
 
 module.exports = async function (fastify, opts) {
-<<<<<<< HEAD
-    fastify.get('/orders', async function (request, reply) {
-        const result = await readall(this.mongo);
-=======
+
     fastify.get('/', async function (request, reply) {
         const result = await readAll(this.mongo);
->>>>>>> 48fc11e7f3e1c324691edab1e0068d91451b697b
+
         
         if(result){
             reply
